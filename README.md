806ade754ef063c4f1ef600b880f067238f4af71
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-12-01 10:50:28 +0000

    Updating to latest FlutterFlow output.

---
+++ README.md
@@ -0,0 +1,7 @@
+# X-MGain
+
+A new Flutter project.
+
+## Getting Started
+
+FlutterFlow projects are built to run on the Flutter _stable_ release.
---
+++ android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.flutterflow.xmgain
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
